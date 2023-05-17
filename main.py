4cf3b41765e7f26280fbc8989f0ad50a0765a79e
García Molina, Jesús <jesus.gm1999@gmail.com>
2023-05-17 10:55:20 +0200

    some more bull

--- main.py
+++ main.py
@@ -2,33 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Points
-x1 = -10
-y1 = 15
-x2 = 20
-y2 = -5
+points = [[0, 0], [5, 0], [0,5], [5,5]]
 
-# Center
+def draw_arc(x1, x2, y1, y2): 
+    # Center
+    cx = (x1 + x2) / 2
+    cy = (y1 + y2) / 2
 
-cx = (x1 + x2) / 2
-cy = (y1 + y2) / 2
+    # Draw points
+    plt.plot(x1, y1, color='blue', marker='o')
+    plt.plot(x2, y2, color='blue', marker='o')
 
-# Draw points
-plt.plot(x1, y1, color='blue', marker='o')
-plt.plot(x2, y2, color='blue', marker='o')
+    # Draw center point
+    plt.plot(cx, cy, color='green', marker='o')
 
-# Draw center point
-plt.plot(cx, cy, color='green', marker='o')
-
-# Radius
-r = np.sqrt(np.power(x1 - cx, 2) + np.power(y1 - cy, 2))
-
-print(r)
-start = np.arctan((y2 - y1) / (x2 - x1)) + np.pi
-end = np.arctan((y1 - cy) / (x1 - cx)) + np.pi
-angles = np.linspace(start, end, 100)
-xs = -r * np.cos(angles) + cx
-ys = -r * np.sin(angles) + cy
-plt.plot(xs, ys, color='red')
+    # Radius
+    r = np.sqrt(np.power(x1 - cx, 2) + np.power(y1 - cy, 2))
+    
+    start = np.arctan((y2 - cy) / (x2 - cx))
+    if x1 == x2 or y1 == y2:
+        end = start - np.pi
+    else:
+        end = start + np.pi
+    print(start)
+    print(end)
+    angles = np.linspace(min(start, end), max(start, end), 100)
+    xs = -r * np.cos(angles) + cx
+    ys = -r * np.sin(angles) + cy
+    plt.plot(xs, ys, color='red')
+    
+for i in range(0, len(points)-1):
+    x1, y1 = points[i]
+    x2, y2 = points[i+1]
+    draw_arc(x1, x2, y1, y2)
+x1, y1 = points[-1]
+x2, y2 = points[0]
+draw_arc(x1, x2, y1, y2)
 plt.show()
 
 """
